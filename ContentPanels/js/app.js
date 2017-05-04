725adc469f90998c5ef715b6b5d197d88a765d3d
meng <wulalameng@gmail.com>
2017-05-03 22:32:21 -0400

    accordion

---
+++ ContentPanels/js/app.js
@@ -0,0 +1,17 @@
+$(
+  function(){
+
+// accordion
+  $(".accordion").on("click", ".accordion-control", function(e){
+    e.preventDefault();
+
+    $(this)
+    .next(".accordion-panel")
+    .not(":animated")
+    .slideToggle();
+  });
+
+
+
+  }
+);
