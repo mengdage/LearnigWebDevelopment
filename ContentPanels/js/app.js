538f8137202b4ea27bdd55afc19812ec5590aa1d
meng <wulalameng@gmail.com>
2017-05-04 11:50:09 -0400

    add tabbed panels

--- ContentPanels/js/app.js
+++ ContentPanels/js/app.js
@@ -12,6 +12,32 @@ $(
   });
 
 
+  // tabbed panels
+  $(".tabbed-panel").each(function(){
+    var $this = $(this);
+    var $tab  = $this.find("li.active");
+    var $link = $tab.find("a");
+    var $panel = $($link.attr("href"));
+
+    $this.on("click", ".tabbed-control", function(e){
+      e.preventDefault();
+      $link = $(this);
+      console.log($link);
+      var id = $link.attr("href");
+      if(id && !$link.is(".active")){
+        $tab.removeClass("active");
+        $panel.removeClass("active");
+
+        $panel = $(id).addClass("active");
+        $tab = $link.parent().addClass("active");
+      }
+
+    });
+
+
+  });
+
+
 
   }
 );
